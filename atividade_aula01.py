bf4b0a6055036c93e719fe2d087e5250b33808c7
danielandrade47 <50267414+danielandrade47@users.noreply.github.com>
2020-08-26 18:24:56 -0300

    Add files via upload

---
+++ atividade_aula01.py
@@ -0,0 +1,19 @@
+produtos = ["Produto 1", "Produto 2", "Produto 3"]
+print(produtos)
+print(produtos[1])
+
+precos = ["25 Reais", "50 Reais", "100 Reais"]
+print(precos)
+print(precos[1])
+
+quantidades = ["3 Unidades", "6 Unidades", "12 Unidades"]
+print(quantidades)
+print(quantidades[1])
+
+
+print (produtos[2])
+print (precos[2])
+print (quantidades[2])
+
+del(produtos[1])
+print(produtos)
