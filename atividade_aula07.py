629cf29bc5d3963f02c69b88ef02038b0c58b53a
danielandrade47 <50267414+danielandrade47@users.noreply.github.com>
2020-10-14 13:05:32 -0300

    Add files via upload

---
+++ atividade_aula07.py
@@ -0,0 +1,18 @@
+class Retangulo:
+    def __init__(self, altura, base, area):
+        self.altura = altura
+        self.base = base
+        self.area = (base * altura)
+
+base = input("Digite a Base do Retangulo: ")
+altura = input("Digite a Altura do Retangulo: ")
+area = float(base) * float(altura)
+
+def imprimir(self):
+    print("Base do Retangulo: " + self.base)
+    print("Altura do Retangulo: " + self.altura)
+    print("Area do Retangulo: " + self.area)
+
+print("Area do Retangulo: ", area)
+print("Altura do Retangulo: " + altura)
+print("Base do Retangulo: " + base)
